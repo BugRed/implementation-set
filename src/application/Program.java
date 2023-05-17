15c2c1d27c74e7c7f9a0c2fc82be36577907e17a
unknown <o.adrian.rodrigues@gmail.com>
2023-05-17 14:46:03 -0300

    My implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,47 @@
 package application;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+import entities.LogAcess;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		Set<LogAcess> set = new HashSet<>();
+
+		
+		
+		System.out.println("How many students for course A?");
+		int numberA = sc.nextInt();
+		for (int i = 1; i <= numberA; i++) {
+			LogAcess LogA = new LogAcess(sc.nextInt());
+			set.add(LogA);
+		}
+		
+		System.out.println("How many students for course B?");
+		int numberB = sc.nextInt();
+		for (int i = 1; i <= numberB; i++) {
+			LogAcess LogA = new LogAcess(sc.nextInt());
+			set.add(LogA);
+		}
+		
+		
+		System.out.println("How many students for course C?");
+		int numberC = sc.nextInt();
+		for (int i = 1; i <= numberC; i++) {
+			LogAcess LogA = new LogAcess(sc.nextInt());
+			set.add(LogA);
+		}
+		for (LogAcess la : set) {
+			System.out.println(la);
+
+		}
+		System.out.println("Total Students: " + set.size());
 
 	}
 
--- src/entities/LogAcess.java
+++ src/entities/LogAcess.java
@@ -1,5 +1,43 @@
 package entities;
 
+import java.util.Objects;
+
 public class LogAcess {
+	private Integer number;
+
+	public LogAcess(Integer number) {
+		this.number = number;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	@Override
+	public String toString() {
+		return "LogAcess [number=" + number + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(number);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LogAcess other = (LogAcess) obj;
+		return Objects.equals(number, other.number);
+	}
+	
 
 }
